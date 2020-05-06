27d172aa2e327cfc73229b5a59f46af5efa60f1e
Tim <sh.kang@ecubelabs.com>
2020-05-06 18:31:45 +0900

    type 문제 해결

--- src/transactional.ts
+++ src/transactional.ts
@@ -1,19 +1,23 @@
 import { Service } from "./service";
 
-export function Transactional() {
-  return async function(target: Service, propertyKey: string, descriptor: PropertyDescriptor) {
-    const originalMethod = descriptor.value;
+export function Transactional<
+  T extends TypedPropertyDescriptor<(...args: any[]) => Promise<any>>
+>() {
+  return (target: Service, propertyKey: string, descriptor: T): T => {
+    return new Promise(() => {
+      const originalMethod = descriptor.value;
 
-    descriptor.value = async function(this: Service, ...args: any[]) {
-      let result: any;
+      descriptor.value = async function (this: Service, ...args: any[]) {
+        let result: any;
 
-      await this.transactionManager.transaction(async () => {
-        result = await originalMethod.apply(this, args);
-      });
+        await this.transactionManager.transaction(async () => {
+          result = await originalMethod?.apply(this, args as any);
+        });
 
-      return result;
-    };
+        return result;
+      };
 
-    return descriptor;
+      return descriptor;
+    }) as any as T;
   };
 }
