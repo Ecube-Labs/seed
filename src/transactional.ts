977f6b4eef12bbeb3881e8b0e66ac3c5c56d41ce
Tim <sh.kang@ecubelabs.com>
2020-05-07 12:09:09 +0900

    transactional 원복

--- src/transactional.ts
+++ src/transactional.ts
@@ -1,23 +1,19 @@
 import { Service } from "./service";
 
-export function Transactional<
-  T extends TypedPropertyDescriptor<(...args: any[]) => Promise<any>>
->() {
-  return (target: Service, propertyKey: string, descriptor: T): T => {
-    return new Promise(() => {
-      const originalMethod = descriptor.value;
+export function Transactional() {
+  return function(target: Service, propertyKey: string, descriptor: PropertyDescriptor) {
+    const originalMethod = descriptor.value;
 
-      descriptor.value = async function (this: Service, ...args: any[]) {
-        let result: any;
+    descriptor.value = async function(this: Service, ...args: any[]) {
+      let result: any;
 
-        await this.transactionManager.transaction(async () => {
-          result = await originalMethod?.apply(this, args as any);
-        });
+      await this.transactionManager.transaction(async () => {
+        result = await originalMethod.apply(this, args);
+      });
 
-        return result;
-      };
+      return result;
+    };
 
-      return descriptor;
-    }) as any as T;
+    return descriptor;
   };
 }
