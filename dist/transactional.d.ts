021fa63e3eb943ed247bc3d9b0c8d990d51f5dec
Tim <sh.kang@ecubelabs.com>
2020-05-07 12:09:42 +0900

    0.0.12

--- dist/transactional.d.ts
+++ dist/transactional.d.ts
@@ -1,2 +1,2 @@
 import { Service } from "./service";
-export declare function Transactional<T extends TypedPropertyDescriptor<(...args: any[]) => Promise<any>>>(): (target: Service, propertyKey: string, descriptor: T) => T;
+export declare function Transactional(): (target: Service, propertyKey: string, descriptor: PropertyDescriptor) => PropertyDescriptor;
