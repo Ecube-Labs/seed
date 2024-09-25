24fe94471459a119ae59f42332d5a4e33269c33d
Lee <seungjun.lee@ecubelabs.com>
2024-09-25 12:37:27 +0900

    chore: added hasInstance for using instanceof state

--- src/error.ts
+++ src/error.ts
@@ -1,4 +1,6 @@
 export abstract class SeedError extends Error {
+  static code = "SEED_ERROR";
+
   constructor(
     message: string,
     public readonly code: string,
@@ -11,16 +13,30 @@ export abstract class SeedError extends Error {
       Error.captureStackTrace(this, this.constructor);
     }
   }
+
+  static [Symbol.hasInstance](instance: any) {
+    if (this === SeedError) {
+      return instance instanceof Error && "code" in instance;
+    } else {
+      return (
+        instance instanceof Error &&
+        "code" in instance &&
+        (instance as any).code === this.code
+      );
+    }
+  }
 }
 
 export class PreAuthorizationFailed extends SeedError {
+  static code = "PRE_AUTHORIZATION_FAILED";
   constructor(message: string, context?: any) {
-    super(message, "PRE_AUTHORIZATION_FAILED", context);
+    super(message, PreAuthorizationFailed.code, context);
   }
 }
 
 export class NoAuthTokenFound extends SeedError {
+  static code = "NO_AUTH_TOKEN_FOUND";
   constructor(message: string, context?: any) {
-    super(message, "NO_AUTH_TOKEN_FOUND", context);
+    super(message, NoAuthTokenFound.code, context);
   }
 }
